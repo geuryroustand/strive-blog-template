42d9fa9a7e9ef901e0926c7fe3c974820d44c4b1
Geury Roustand <theromantic2010@hotmail.com>
2021-08-11 09:13:08 +0200

    SERVER CREATED

--- src/data/authors.js
+++ src/data/authors.js
@@ -1,9 +1,50 @@
 import express from "express";
+import uniqid from "uniqid";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 const authorRouter = express.Router();
 
-authorRouter.get((req, res) => {
-  res.send("get working");
+const currentFilePath = fileURLToPath(import.meta.url);
+const currentFolderPath = dirname(currentFilePath);
+const authorJsonPath = join(currentFolderPath, "posts.json");
+
+authorRouter.post("/", (req, res) => {
+  const newAuthor = {
+    ...req.body,
+    id: uniqid(),
+    avatar: ` https://ui-avatars.com/api/?name=John+Doe`,
+  };
+
+  const authors = JSON.parse(fs.readFileSync(authorJsonPath));
+
+  authors.push(newAuthor);
+
+  fs.writeFileSync(authorJsonPath, JSON.stringify(authors));
+
+  res.status(201).send({ id: newAuthor.id });
+});
+
+authorRouter.get("/", (req, res) => {
+  const arrFile = fs.readFileSync(authorJsonPath);
+  res.send(JSON.parse(arrFile));
+});
+
+authorRouter.get("/:id", (req, res) => {
+  const arrfile = JSON.parse(fs.readFileSync(authorJsonPath));
+
+  const authorsFilter = arrfile.find((arrs) => arrs.id === req.params.id);
+  res.send(authorsFilter);
+});
+
+authorRouter.delete("/:id", (req, res) => {
+  const authors = JSON.parse(fs.readFileSync(authorJsonPath));
+
+  const ramainAuthors = authors.filter((auth) => auth.id !== req.params.id);
+  fs.writeFileSync(authorJsonPath, JSON.stringify(ramainAuthors));
+
+  res.status(204).send();
 });
 
 export default authorRouter;
--- src/server.js
+++ src/server.js
@@ -1,17 +1,14 @@
 import express from "express";
 import cors from "cors";
-// import authorRouter from "./data/authors";
+import authorRouter from "../src/data/authors.js";
 const server = express();
 
 const port = 3001;
 
-// server.use(express.json());
+// server.use(cors());
+server.use(express.json());
 
-// server.use("/", authorRouter);
-
-server.get("/", (req, res) => {
-  res.send("Get post");
-});
+server.use("/authors", authorRouter);
 
 server.listen(port, () => {
   console.log("Server listing port");
